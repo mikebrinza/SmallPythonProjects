232e4e7112fa345d2e040d40076bae17e16eecc0
mikebrinza <mpascari@gmail.com>
2021-11-11 05:47:50 +0200

    code clean-up

--- fileMover/fileMover.py
+++ fileMover/fileMover.py
@@ -2,50 +2,44 @@
 #using an extension of your choice to a specified Destination folder.
 
 import os
-import random
 import shutil
-from pathlib import Path
+from tkinter import *
 from tkinter import filedialog
 from tkinter import messagebox
-from tkinter import *
 from tkinter import simpledialog
 
-#initialize Tkinter for folder selection
+#initialize Tkinter for folder selection and extension input
 ws = Tk()
 ws.withdraw()
 
 def main():
 
+    #asking for input folder and extension
     askMsg("Select SOURCE folder")
     sourceFolder = filedialog.askdirectory()
     askMsg("Select DESTINATION folder")
     destinationFolder = filedialog.askdirectory()
-    fileExtension = askString()
     
+    fileExtension = askString()
+    srcPath = sourceFolder
+    srcDest = destinationFolder
 
+    #for debug purposes
     print(sourceFolder)
     print(destinationFolder)
     print(fileExtension)
 
-    srcPath = sourceFolder
-    srcDest = destinationFolder
-    # srcPath = r'D:\xG\Heroes of Might and Magic 3 Complete\Maps\maps6000\1 - The Restoration of Erathia\1 - Small'
-    # srcDest = r'D:\xG\Heroes of Might and Magic 3 Complete\Maps\maps6000\1 - The Restoration of Erathia'
-
+    #file mover algorithm
     for root, dirs, files in os.walk(srcPath):
         for name in files:
             if name.endswith(fileExtension):
                 try:
                     filePath = root + os.sep + name
                     filePathDestination = srcDest + os.sep + name
-                    #Path(filePath).rename(filePathDestination)
                     shutil.move(filePath, filePathDestination)
                     print("Successfully moved: ", name)
-                    #Path(filePath).rename(filePathDestination)
-                    #shutil.move(filePath,srcDest)
                 except UnicodeEncodeError:
                     print("-----------------Error")
-        
 
 def askMsg(message: str):
     response = messagebox.askokcancel('information', message)
